16b32c21cecffe08aa1dba8b51e35a961570e02c
uonele <uoneleu@163.com>
2022-04-07 01:07:56 +0800

    2022-04-07 01:07:50

--- 25.k-个一组翻转链表.java
+++ 25.k-个一组翻转链表.java
@@ -20,7 +20,7 @@ class Solution {
     Accepted
     62/62 cases passed (3 ms)
     Your runtime beats 5.72 % of java submissions
-    Your memory usage beats 5.1 % of java submissions (41.6 MB)
+    Your memory usage beats 37.15 % of java submissions (41.1 MB)
     */
     public ListNode reverseKGroup(ListNode head, int k) {
         ListNode res = new ListNode(0);
@@ -29,6 +29,7 @@ class Solution {
         Stack<ListNode> s = new Stack<ListNode>();
         while(cur != null){
             int i = 0;
+            // 这里要记录一下cur节点的位置
             ListNode temp = cur;
             
             while(i < k && cur != null){
@@ -36,7 +37,7 @@ class Solution {
                 s.push(cur);
                 cur = cur.next;
             }
-
+			//最后一轮时节点个数小于k，直接把剩余节点的头结点赋值给res即可。
             if(i < k){
                 res.next = temp;
                 break;
@@ -45,9 +46,11 @@ class Solution {
                 res.next = s.pop();
                 res = res.next;
             }
+            // 这个必须有，因为 3-》4翻转后变成了4-》3，但3的next还是4，就成了无限循环，会爆超时错误。
             res.next = null;
         }
         return resHead.next;
+    
     }
 }
 // @lc code=end
